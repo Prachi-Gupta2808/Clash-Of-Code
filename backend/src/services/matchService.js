98c6c1515e77f60385fa4f60ba924d96be4ee9ba
Prachi-Gupta2808 <prachig2808@gmail.com>
2026-04-17 00:16:12 +0530

    fixed rating wise question fetching

--- backend/src/services/matchService.js
+++ backend/src/services/matchService.js
@@ -7,21 +7,30 @@ exports.createMatch = async (
   player2_ID,
   roomId,
   mode,
-  isChallenged = false
+  isChallenged = false,
+  player1_rating = 0,
+  player2_rating = 0
 ) => {
   const questionCount = mode === "mcq" ? 10 : mode === "predict" ? 5 : 1;
 
+  let matchQuery = { theme: mode };
+
+  if (mode === "contest") {
+    const avgRating = (player1_rating + player2_rating) / 2;
+    matchQuery.rating = Math.ceil(avgRating / 100) * 100;
+  }
+
   const questions = await Question.aggregate([
-    { $match: { theme: mode } },
+    { $match: matchQuery },
     { $sample: { size: questionCount } },
   ]);
 
   if (!questions.length) {
     throw new Error("No questions found for this theme");
   }
-  const now = new Date();
 
-  const duration = mode === "mcq" ? 10 : 15 ; // minutes
+  const now = new Date();
+  const duration = mode === "mcq" ? 10 : 15;
 
   const newMatch = await Match.create({
     player1: player1_ID,
